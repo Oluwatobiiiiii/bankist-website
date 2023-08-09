9f1ba1b88a23a0d129270da65ea68da8cef95a8c
Treasure <odetokuntreasure6@gmail.com>
2023-08-09 14:05:48 +0100

    implemented image-lazy-loading , slider and tab

--- script.js
+++ script.js
@@ -233,6 +233,95 @@ const imgObserver = new IntersectionObserver(loadingImg,{
 
 imgTargets.forEach(img => imgObserver.observe(img))
 
+//slider
+const slider = function(){
+const slides = document.querySelectorAll('.slide')
+const slider = document.querySelector('.slider')
+const btnLeft = document.querySelector('.slider__btn--left')
+const btnRight = document.querySelector('.slider__btn--right')
+const dotContainer = document.querySelector('.dots')
+
+
+
+let curSlide = 0;
+const maxSlide = slides.length
+
+const createDots = function(){
+  slides.forEach(function(_,i){
+    dotContainer.insertAdjacentHTML('beforeend', 
+    `<button class="dots__dot" data-slide="${i}"></button>
+    `)
+  })
+}
+
+
+const activateDot = function(slide){
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'))
+  document.querySelector(`.dots__dot[data-slide = "${slide}"]`).classList.add('dots__dot--active')
+}
+
+
+
+const goToSlide = function(slide){
+  slides.forEach((s,i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`))
+}
+
+
+
+//next slide
+const nextSlide = function(){
+  if (curSlide === maxSlide - 1 ){
+    curSlide = 0
+   }else{
+    curSlide++;
+   }
+   goToSlide(curSlide)
+   activateDot(curSlide)
+}
+
+
+const prevSlide = function(){
+  if(curSlide === 0){
+    curSlide = maxSlide -1;
+  }else{
+  curSlide--;
+  }
+  goToSlide(curSlide);
+  activateDot(curSlide)
+}
+
+const init = function(){
+  goToSlide(0)
+  createDots();
+  activateDot(0)
+}
+init();
+
+
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
+
+document.addEventListener('keydown', function(e){
+  console.log(e)
+  if(e.key === 'ArrowLeft')prevSlide()
+  if(e.key === 'ArrowRight')nextSlide()
+  activateDot(curSlide)
+})
+
+
+dotContainer.addEventListener('click', function(e){
+  if(e.target.classList.contains('dots__dot')){
+    console.log('dot')
+    const {slide} = e.target.dataset;
+    goToSlide(slide)
+    activateDot(curSlide)
+    
+  }
+})
+}
+
+slider();
+//button slider
 
 
 //////////////////////////////
@@ -362,3 +451,9 @@ console.log(h1.parentElement.children);
  });
 
 */
+
+
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('html',e)
+})
+
